62196c60fa6c158308322464f906abee3ae3e9c3
Ethan MacGregor <ethanbrs1@gmail.com>
2018-01-26 16:42:54 -0500

    Added my method. No idea how to test it though.

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,11 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
+import java.util.ArrayList;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -31,14 +33,20 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+		Random randy = new Random();
+		String outString = "";
+		ArrayList charArr = new ArrayList();
+		for (int i = 0; i < input.length(); i++) {
+			outString = outString + charArr.get(randy.nextInt(charArr.length));
+		}
+	 	return outString;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
