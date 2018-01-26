e5883370acf852e5ce1517024d675d8020e1e44b
DomKynkor <domkynkor@gmail.com>
2018-01-26 16:33:46 -0500

    Added calc method
    Added calc method implementation

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,6 +43,10 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  //Person 3 put your implementation here
-	  return null;
+	  String myString = "";
+	  for (int i = 0; i < input.length(); i++) {
+		myString = input.charAt(i) + myString;
+	  }
+	  return myString;
 	}
 }
