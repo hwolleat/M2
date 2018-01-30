8e281c37165137927af5dd47042747b134e689e4
Ethan MacGregor <ethanbrs1@gmail.com>
2018-01-30 14:19:51 -0500

    Fixed Person2.java calc method

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -34,17 +34,18 @@ public class Person2 {
 	private String calc(String input) {
 	  //Person 2 put your implementation here
 		Random randy = new Random();
-		String outString = "";
-		ArrayList charArr = new ArrayList();
-		for (int i = 0; i < input.length(); i++) {
-			charArr.add(input.charArr());
-		}
-		for (int i = 0; i < input.length(); i++) {
-			int randomInt = randy.nextInt(charArr.length);
-			outString = outString + charArr.get(randomInt);
-			charArr.remove(randomInt);
-		}
-	 	return outString;
+        String outString = "";
+        char[] charArr = input.toCharArray();
+        ArrayList<Character> charArrList = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            charArrList.add(charArr[i]);
+        }
+        for (int i = 0; i < input.length(); i++) {
+            int randomInt = randy.nextInt(charArrList.size());
+            outString = outString + charArrList.get(randomInt);
+            charArrList.remove(randomInt);
+        }
+        return outString;
 	}
 	/**
 	 * Return a string rep of this object
