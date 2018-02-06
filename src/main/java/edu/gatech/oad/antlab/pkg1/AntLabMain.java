0ddc33a58af2cf3620b6c6c7175ce6cf529f261b
Ethan MacGregor <ethanbrs1@gmail.com>
2018-02-06 16:00:19 -0500

    Added Sas to end of Diana's name.

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,11 +67,11 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Dominic Kynkor");
 		  toPrint += p3.toString("dkynkor3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Diana ");
-                  toPrint += p4.toString("dsas3");
-                  
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("Diana Sas");
+          toPrint += p4.toString("dsas3");
+
 
         System.out.println(toPrint);
 
